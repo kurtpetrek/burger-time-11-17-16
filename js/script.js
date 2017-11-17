d85e3af64ac7a9deb4be40c75f5e83b715425d84
Kurt Petrek <kurtpetrek@gmail.com>
2017-11-17 18:44:16 -0500

    adds js for nav

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var navToggle = document.querySelector("#main-nav-toggle");
+var navList = document.querySelector("#main-nav-list");
+
+// Always check to see if elements exist before writing js involving those elements
+if (navToggle && navList) {
+  navToggle.addEventListener("click", function() {
+    // This checks to see if the element has the class of close on it
+    if (navToggle.classList.contains("close")) {
+      // if the element has the close class that means the nav is open so to close it we need to remove the open class from the nav list and remove the close class from the hamburger
+      navToggle.classList.remove("close");
+      navList.classList.remove("open");
+    } else {
+      navToggle.classList.add("close");
+      navList.classList.add("open");
+    }
+  });
+}
